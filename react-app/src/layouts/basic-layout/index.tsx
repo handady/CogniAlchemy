d2c26278ceab10c4f44f93102b3466c20cb5d74a
Hans <162235990@qq.com>
2025-02-06 17:12:56 +0800

    使用vite创建项目

---
+++ react-app/src/layouts/basic-layout/index.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import React from "react";
+
+const Test: React.FC = () => {
+  return (
+    <div className="text-xl underline" style={{ color: "red" }}>
+      Hello World
+    </div>
+  );
+};
+
+export default Test;
