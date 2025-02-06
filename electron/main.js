6ccdbeaab259762c3ea2587a14fba6fa03cf78b2
Hans <162235990@qq.com>
2025-02-06 11:38:10 +0800

    Initial commit

---
+++ electron/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // 开发时加载 React 开发服务器地址，打包时加载打包后的文件
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    `file://${path.join(__dirname, "../react-app/build/index.html")}`;
+  win.loadURL(startUrl);
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ electron/preload.js
@@ -0,0 +1,4 @@
+// preload.js
+window.addEventListener("DOMContentLoaded", () => {
+  // 可在此处预加载一些 API 或进行初始化操作哦~
+});
